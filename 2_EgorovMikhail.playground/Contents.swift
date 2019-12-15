e148ee4ae7a118803a83d6edb714f5835120ec99
egorovmikhail <egorovmikhail@bk.ru>
2019-12-15 23:40:14 +1100

    В задании № 2 изменил тип функции и способ вывода

--- 2_EgorovMikhail.playground/Contents.swift
+++ 2_EgorovMikhail.playground/Contents.swift
@@ -19,15 +19,18 @@ import UIKit
  evenOrNot(numeric: 15)
  
   /* Задание № 2. Написать функцию, которая определяет, делится ли число без остатка на 3. */
- func dividedByThree (numeric: Int) {
+ func dividedByThree (numeric: Int) -> String {
+    var text: String
     if numeric % 3 == 0 {
-        print("Задание № 2 \n Число \(numeric) делится без остатка на 3 \n")
+        text = "Число \(numeric) делится без остатка на 3 \n"
     } else {
-        print("Задание № 2 \n Число \(numeric) не делится без остатка на 3 \n")
+        text = "Число \(numeric) не делится без остатка на 3 \n"
     }
+    return text
  }
- dividedByThree(numeric: 128)
-
+ let t: String = dividedByThree(numeric: 3)
+ print("Задание № 2 \n", dividedByThree(numeric: 2), t)
+ 
  /* Задание № 3 Создать возравстающий массив из 100 чисел. */
  var Array: [Int] = []
  for i in 1...100 {
@@ -37,7 +40,7 @@ import UIKit
  
  /* Задание № 4 Удалить из массива все чётные числа и все числа, которые не делятся на 3. */
  for (_, value) in Array.enumerated() {
-    if (value % 2) == 0 || (value % 3) == 0 {
+    if (value % 2) == 0 || (value % 3) != 0 {
         Array.remove(at: Array.firstIndex(of: value)!)
     }
  }
