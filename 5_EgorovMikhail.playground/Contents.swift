93fc0c08e70b98d69a2e12abb2c720baeff6c548
egorovmikhail <egorovmikhail@bk.ru>
2020-01-22 21:26:30 +1100

    Добавил расширение в протокол Car

--- 5_EgorovMikhail.playground/Contents.swift
+++ 5_EgorovMikhail.playground/Contents.swift
@@ -20,13 +20,99 @@
 
 import UIKit
 
+enum YearState: Int {
+    case _1970 = 1970
+    case _1980 = 1980
+    case _1990 = 1990
+    case _2000 = 2000
+}
+//    Перечисления запуска/остановки двигателя
+enum EngineState: String {
+    case start = "двигатель запущен"
+    case stop = "двигатель заглушен"
+}
+//    Перечисления открыть/закрыть окна
+enum WindowState: String {
+    case open = "окна открыты"
+    case close = "окна закрыты"
+}
+//    Перечисления открыть/закрыть двери
+enum DoorState: String {
+    case open = "двери открыты"
+    case close = "двери закрыты"
+}
+
+
 protocol Car {
-        //    Свойства
-    var brand: String { get set }
-    var model: String { get set }
-    var year: String { get set }
-    var engine: String { get set }
-    var window: String { get set }
+//    Свойства
+    var brand: String { get }
+    var model: String { get }
+    var year: YearState { get }
+    var engine: EngineState { get set }
+    var windows: WindowState { get set }
+    var door: DoorState { get set }
+    
+    init(brand: String, model: String, year: YearState, engine: EngineState, windows: WindowState, door: DoorState)
     
 }
+
+extension Car {
+    mutating func StopSartEngin() {
+        switch engine {
+            case .start:
+                return self.engine = EngineState.stop
+            case .stop:
+                return self.engine = EngineState.start
+        }
+    }
+    mutating func OpenCloseWindows() {
+        switch windows {
+            case .close:
+                return self.windows = WindowState.open
+            case .open:
+                return self.windows = WindowState.close
+        }
+    }
+    mutating func OpenCloseDoor() {
+        switch door {
+            case .close:
+                return self.door = DoorState.open
+            case .open:
+                return self.door = DoorState.close
+        }
+    }
+}
+
+class TrunkCar: Car {
+    
+    var brand: String
+    var model: String
+    var year: YearState
+    var engine: EngineState
+    var windows: WindowState
+    var door: DoorState
+    
+    required init(brand: String, model: String, year: YearState, engine: EngineState, windows: WindowState, door: DoorState) {
+        self.brand = brand
+        self.model = model
+        self.year = year
+        self.engine = engine
+        self.windows = windows
+        self.door = door
+    }
+    
+    
+}
+
+
+//class SportCar {
+//}
+
+var car = TrunkCar(brand: "Mitsubishi", model: "Sprinter", year: ._1970, engine: .stop, windows: .close, door: .close)
+
+car.StopSartEngin()
+print(car.engine.rawValue)
+car.StopSartEngin()
+print(car.engine.rawValue)
+
 
