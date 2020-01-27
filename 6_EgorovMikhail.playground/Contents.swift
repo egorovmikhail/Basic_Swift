795150d40ac3ca932ec28b049ca04a0c490287c8
egorovmikhail <egorovmikhail@bk.ru>
2020-01-27 17:53:52 +1100

    Создал коллекцию типа очередь

--- 6_EgorovMikhail.playground/Contents.swift
+++ 6_EgorovMikhail.playground/Contents.swift
@@ -1,5 +1,5 @@
 /*
- 5_EgorovMikhail.playground
+ 6_EgorovMikhail.playground
  Created by Михаил Егоров on 23.01.2020.
 
     Домашнее задание
@@ -9,4 +9,31 @@
  */
 
 import UIKit
+
+struct Queue<Element> {
+    var items = [Element]()
+    mutating func push(_ item: Element) {
+        items.append(item)
+    }
+    mutating func pop() -> Element {
+        return items.removeFirst()
+    }
+}
+
+
+var name = Queue<String>()
+var numer = Queue<Int>()
+
+
+name.push("Masha")
+name.push("Dasha")
+name.push("Olga")
+//name.pop()
+
+numer.push(12)
+numer.push(45)
+numer.push(38)
+
+print(name.items)
+print(numer.items)
 
